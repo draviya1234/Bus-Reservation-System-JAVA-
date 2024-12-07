7fedbc64f3ebf0dbb3b7e956d7bbf430040c4e73
Draviya <145901814+draviya1234@users.noreply.github.com>
2024-12-07 12:13:14 +0530

    Add files via upload

---
+++ BusReservation.java
@@ -0,0 +1,40 @@
+package busres;
+
+import java.util.Scanner; //nested package/ hierarchical package
+import java.util.ArrayList;
+
+public class BusReservation {
+
+	public static void main(String[] args) {
+		
+	
+		ArrayList<Vechile1> buses = new ArrayList<Vechile1>();
+		ArrayList<Booking> bookings = new ArrayList<Booking>();
+		
+		buses.add(new Vechile1(1,true,2));
+		buses.add(new Vechile1(2,false,50));
+		buses.add(new Vechile1(3,true,48));
+		
+		int userOpt = 1;
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Vechile1 b:buses) {
+			b.displayBusInfo();
+		}
+		
+		while(userOpt==1) {
+			System.out.println("Enter 1 to Book and 2 to exit");
+			userOpt = scanner.nextInt();
+			if(userOpt == 1) {
+				Booking booking = new Booking();
+				if(booking.isAvailable(bookings,buses)) {
+					bookings.add(booking);
+					System.out.println("Your booking is confirmed");
+				}
+				else
+					System.out.println("Sorry. Bus is full. Try another bus or date.");
+			}
+		}
+	}
+
+}
